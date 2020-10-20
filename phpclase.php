a200045ae715ccb14a3ed0eb4d8703f45de3fa1d
unknown <mbusquets11372@alumnes.iesmanacor.cat>
2020-10-20 18:49:57 +0200

    Vectors_PHP_exercises_to_learn_1_MARTI

---
+++ phpclase.php
@@ -0,0 +1,12 @@
+<?php
+$weekdays[0]="Monday";
+$weekdays[1]="Tuesday";
+$weekdays[2]="Wednesday";
+$weekdays[3]="Thursday";
+$weekdays[4]="Friday";
+$weekdays[5]="Saturday";
+$weekdays[6]="Sunday";
+for($i=0 ; $i<count($weekdays) ; $i++)
+
+echo "The day we are on is $weekdays[$i] \n";
+?>
