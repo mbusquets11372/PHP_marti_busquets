85af265d3e5a0b271b5e194240cff1a5a01bb9ae
mbusquets11372 <72940731+mbusquets11372@users.noreply.github.com>
2020-11-03 12:33:49 +0100

    Update Vectors_PHP_exercices_3_MBusquets.php

--- Vectors_PHP_exercices_3_MBusquets.php
+++ Vectors_PHP_exercices_3_MBusquets.php
@@ -31,28 +31,25 @@ foreach ($vecto2 as $value2) {
 
 //3er vector
 echo "<br><strong>3 Vector: Times when an alarm sounds</strong><br>";
-$vector3[]=array("8", "12", "14", "16", "20");
+$vector3=array("8", "12", "14", "16", "20");
 
-foreach ($vector3 as $value_3) {
-  foreach ($value_3 as $value_4) {
-  echo "$value_4 <br>";}
-}
-        //Posam es contador aqui perque mes adalt sa variable value_3 no existeix.
-$counts3=sizeof($value_3);
-
+$counts3=sizeof($vector3);
 echo "Number of elements: $counts3<br>";
+
+foreach ($vector3 as $value_3){
+  echo "$value_3 <br>";
+}
+
 
 //4t vector
 echo "<br><strong>4 Vector: List of components</strong><br>";
-$vector4[]=array("PC", "Mouse", "Keyboard", "i3core", "8GB RAM", "1TB");
+$vector4=array("PC", "Mouse", "Keyboard", "i3core", "8GB RAM", "1TB");
 
-foreach ($vector4 as $value_5) {
-  foreach ($value_5 as $value_6) {
-  echo "$value_6 <br>";}
-}
-        //Posam es contador aqui perque mes adalt sa variable value_5 no existeix.
-$counts4=sizeof($value_5);
-
+$counts4=sizeof($vector4);
 echo "Number of elements: $counts4<br>";
+
+foreach ($vector4 as $value_5){
+  echo "$value_5 <br>";
+}
 
 ?>
