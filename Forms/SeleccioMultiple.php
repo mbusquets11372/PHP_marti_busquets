9e9761f3e37d0d09a3456c515a430e8e854b9d7d
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2020-11-10 19:13:26 +0100

    Forms_Marti

---
+++ Forms/SeleccioMultiple.php
@@ -0,0 +1,9 @@
+<?php
+// POST: No mostra res a nes navegador
+// GET: Mostra contingut a nes navegador
+      echo "$_GET[nom] <br>";
+      echo "$_GET[email] <br>";
+
+      foreach ($_GET['navegadors'] as $opcio)
+                      echo $opcio . "\n";
+?>
---
+++ Forms/contesta.php
@@ -0,0 +1,6 @@
+<?php
+// POST: No mostra res a nes navegador
+// GET: Mostra contingut a nes navegador
+       echo "$_GET[nom]";
+
+      ?>
