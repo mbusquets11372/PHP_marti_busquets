0361beda046442d796e5fa16f789695598cea357
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2020-11-19 20:46:45 +0100

    form_intermediate_validation.php

---
+++ Function/form_intermediate_validation.php
@@ -0,0 +1,28 @@
+<?php
+//-------------1----------------//
+echo "<b>Function 1</b><br>";
+function emailValidation($email)
+{
+return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+if (emailValidation("martibp2000@gmail.com"))
+{
+  echo "El correu és valid<br><br>";
+}
+else
+{
+  echo "El correu no és valid<br><br>";
+}
+
+//-------------2----------------//
+echo "<b>Function 2</b><br>";
+function passwordValidation($pwd1,$pwd2)
+{
+  if (strcmp($pwd1,$pwd2) ==0)
+  return true;
+  return false;
+}
+
+echo passwordValidation('avemaria', 'avemaria');
+?>
