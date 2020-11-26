d3ebc529f8a096f7fe1e84608ebc12aba292bcc9
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2020-11-26 19:33:49 +0100

    form_intermediate_2_validation.php

---
+++ Function/form_intermediate2_validation.php
@@ -0,0 +1,23 @@
+<?php
+//---------------------1-----------------------//
+$nom="Marti Busquets Prohens";
+function fullNameSplit($nom)
+{
+  return explode (" ",$nom);
+}
+$res=fullNameSplit($nom);
+foreach ($res as $res2)
+  {
+  echo $res2, "<br>";
+  }
+
+echo "<hr>";
+//---------------------2-----------------------//
+
+function pwdHash($password)
+{
+return password_hash($password, PASSWORD_DEFAULT);
+}
+
+echo pwdHash("patatatata");
+?>
