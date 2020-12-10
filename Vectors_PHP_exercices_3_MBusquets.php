190edd3e9e54f8ceabb29aaeacd5fd2a13a07061
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2020-12-10 22:05:52 +0100

    Corregit Vectors_PHP_3

--- Vectors_PHP_exercices_3_MBusquets.php
+++ Vectors_PHP_exercices_3_MBusquets.php
@@ -7,11 +7,11 @@
 //Les posicions als vectors / matrius comencen en 0, sino emprem aquesta posició es un espai de memoria que s'esta desperdiciant.
 //1er vector
 echo "<strong>1 Vector: window time of a day</strong><br>";
-$vecto1["0"]="Morning"; // visusi corrections 2 - KO
-$vecto1["1"]="Noon";
-$vecto1["2"]="Afternoon";
-$vecto1["3"]="Evening";
-$vecto1["4"]="Night";
+$vecto1[0]="Morning"; // visusi corrections 2 - KO
+$vecto1[1]="Noon";
+$vecto1[2]="Afternoon";
+$vecto1[3]="Evening";
+$vecto1[4]="Night";
 
 $counts1=sizeof($vecto1);
 
@@ -23,9 +23,9 @@ foreach ($vecto1 as $value1) {
 
 //2n vector
 echo "<br><strong>2 Vector: Dishes of a restaurant</strong><br>";
-$vecto2["0"]="Beef with mushroom sauce";//visusi corrections 2 - KO
-$vecto2["1"]="Spaghetti carbonara";
-$vecto2["2"]="Spanish omelette";
+$vecto2[0]="Beef with mushroom sauce";//visusi corrections 2 - KO
+$vecto2[1]="Spaghetti carbonara";
+$vecto2[2]="Spanish omelette";
 
 $counts2=sizeof($vecto2);
 
@@ -38,7 +38,7 @@ foreach ($vecto2 as $value2) {
 //3er vector
 echo "<br><strong>3 Vector: Times when an alarm sounds</strong><br>";
 
-$vector3=array("8", "12", "14", "16", "20");// visusi corrections 2 - KO
+$vector3=array(8, 12, 14, 16, 20);// visusi corrections 2 - KO
                                                                    //visusi - si aquest fora el multidimensional, seria millor posar cada element en una posicio diferent
                                                                     //visusi - exemple - $vector3[]=8; $vector3[]=12; $vector3[]=14; ; $vector3[]=16; $vector3[]=20;
                                                                     //visusi - si emprem instrucció array("8", "12", "14", "16", "20")   no caldria posar els corchetes a $vector3[]
