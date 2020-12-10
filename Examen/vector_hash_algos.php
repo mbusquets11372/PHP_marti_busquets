e7709facf917e4f33a91a46fbde2f63127ece522
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2020-12-10 20:58:25 +0100

    examen vector

---
+++ Examen/vector_hash_algos.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Donada la funcio de php hash_algos()
+1-Mostra amb un bucle for each tots els seus elements
+2- de manera ordenada alfabeticament
+3- I mostra amb accés directe per posició als elements amb els valors:
+    sha256
+    md5
+mes informació al link:
+https://www.php.net/manual/en/function.hash-algos.php
+......
+*/
+//------------1------------//
+foreach(hash_algos() as $m)
+{
+echo "<option value=\"".$m."\">".$m."</option>";
+}
+//------------2------------//
+
+ ?>
